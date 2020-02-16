f26a6da2a7cec9425c3a0491cfed75e7d9db7712
smkim <smkim@iotcube.com>
2020-02-16 15:06:11 +0900

    2.b_Reactivity/Declarations
    - add reactive declarations

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,8 @@
 <script>
 	let count = 0;
 
+	$: doubled = count * 2;
+
 	function handleClick() {
 		count += 1;
 	}
@@ -8,7 +10,9 @@
 
 <main>
 	<button on:click={handleClick}>
-	Clicked {count} {count === 1 ? 'time' : 'times' }</button>
+		Clicked {count} {count === 1 ? 'time' : 'times' }
+	</button>
+	<p>{count} doubled is {doubled}</p>
 </main>
 
 <style>
