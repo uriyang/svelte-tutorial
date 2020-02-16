990ed31623af5b7748c9022ab9c7bbab7516cedb
smkim <smkim@iotcube.com>
2020-02-16 15:46:23 +0900

    3.c_Props/Spread props
    - spread props

--- src/App.svelte
+++ src/App.svelte
@@ -1,10 +1,22 @@
 <script>
-	import Nested from './Nested.svelte';
+	import Info from './Info.svelte';
+
+	const pkg = {
+		name: 'svelte',
+		version: 3,
+		speed: 'blazing',
+		website: 'https://svelte.dev'
+	}
 </script>
 
 <main>
-	<Nested answer={42} />
-	<Nested />
+	<!-- <Info
+		name={pkg.name}
+		version={pkg.version}
+		speed={pkg.speed}
+		website={pkg.website}
+	/> -->
+	<Info {...pkg} />
 </main>
 
 <style>
